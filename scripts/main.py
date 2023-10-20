f8c5e3190723e12004ea28d96e66b8d7c89563b8
KarstnAncheta <92937999+KarstnAncheta@users.noreply.github.com>
2023-10-20 11:27:11 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -16,20 +16,149 @@
          \/\_____\  \ \_____\  \ \_\ \_\  \ \_\  \ \_\      \ \_\
           \/_____/   \/_____/   \/_/ /_/   \/_/   \/_/       \/_/
 
-Simple script that reads file from GitHub Projects and outputs the following:
- - Figure of the actual timeline of the project in .png format.
- - Statistics of system's progress in .csv format.
+Simple script that reads a file from GitHub Projects and outputs the following:
+ - Projected timeline in .png format.
+ - Actual timeline in .png format.
+ - Simple statistics of system's progress in .csv format.
+ - Accomplishment Report.
 """
 import os
 import shutil
 import math
 from pathlib import Path
-from datetime import date
+from datetime import date, datetime
+import calendar
 import pandas as pd
 import plotly.express as px
+from docxtpl import DocxTemplate, InlineImage
 
-PROJECT_NAME = 'egov-bebs'
+PROJECT_NAME = 'eGov-BEBS'
+PROJECT_TSV_FILE_PATH = 'egov-bebs.tsv'
+PROJECT_DETAILS = 'To design, build, test, and deploy a system for the Budgeting Office that will digitize a part of their current process.'
+DEVELOPER_DETAILS = {
+    'name': 'Karstn Paul P. Ancheta',
+    'position': 'Information Systems Analyst I',
+    'date_hired': datetime(2023, 1, 16)
+}
+RENDER_DATES = [
+    datetime(2023, 10, 16)
+]
+REVIEWER_DETAILS = {
+    'name': 'Patrick V. Vinluan',
+    'position': 'Information Systems Analyst III',
+}
 DIRECTORY_SEPARATOR = os.path.sep
+
+
+def generate_accomplishment_report(data_frame: pd.DataFrame, project_name: str) -> None:
+    """
+    Generates Accomplishment Report by getting data from the project's tsv file.
+
+    :param data_frame:
+    :param project_name:
+    :return:
+    """
+
+    def get_cutoff_period():
+        # Get the current date
+        current_date = datetime.now()
+
+        # Determine the first day of the current month
+        first_day_of_month = current_date.replace(day=1)
+
+        # Determine the last day of the current month
+        last_day_of_month = calendar.monthrange(current_date.year, current_date.month)[1]
+
+        # Determine the cutoff period based on the current date
+        if current_date.day <= 15:
+            cutoff_start = first_day_of_month.strftime("%Y-%m-01")
+            cutoff_end = first_day_of_month.strftime("%Y-%m-15")
+        else:
+            cutoff_start = first_day_of_month.strftime("%Y-%m-16")
+            cutoff_end = first_day_of_month.replace(day=last_day_of_month).strftime("%Y-%m-%d")
+
+        return f"{cutoff_start} to {cutoff_end}"
+
+    def calculate_cutoffs(start_date, current_date):
+        cutoffs = 0
+        # Calculate complete months between start_date and current_date
+        complete_months = (current_date.year - start_date.year) * 12 + current_date.month - start_date.month
+
+        # Count the number of cutoffs in complete months
+        cutoffs += complete_months * 2
+
+        # Check if the start_date and current_date are within the same month
+        if start_date.day <= 15 and current_date.day > 15:
+            cutoffs += 1
+        elif start_date.day > 15 and current_date.day > 15:
+            cutoffs += 1
+
+        return cutoffs
+
+    # Get the current date and time
+    current_datetime = datetime.now()
+
+    # Extract year, month, and day from the current date
+    current_year = current_datetime.year
+    current_month = current_datetime.month
+    current_day = current_datetime.day
+
+    # Create a datetime object with the extracted year, month, and day
+    formatted_datetime = datetime(current_year, current_month, current_day)
+
+    # Drop unnecessary columns
+    data_frame = data_frame.drop([
+        'URL',
+        'Story Points',
+        'Labels',
+        'Priority Level',
+    ], axis=1)
+
+    # Get tasks under the "In Progress" and "Done" column
+    tasks = data_frame[(data_frame['Status'] == 'In Progress') | (data_frame['Status'] == 'Done')]
+
+    # DOCUMENT TEMPLATE
+    doc = DocxTemplate('AR-template.docx')
+
+    # Specify the directory containing the images
+    directory_path = "screenshots"
+
+    # Get a list of all files in the directory
+    all_files = os.listdir(directory_path)
+
+    # Filter out image files (you can extend the list of allowed extensions)
+    image_files = [file for file in all_files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+    # Process each image file
+    image_data_array = []
+    for image_file in image_files:
+        image_path = os.path.join(directory_path, image_file)
+        image_data = InlineImage(doc, image_path, width=512 * 9525, height=380 * 9525)  # EMU calculations
+        image_data_array.append(image_data)
+
+    data = {
+        'project_name': project_name,
+        'project_objectives': PROJECT_DETAILS,
+        'developer_name': DEVELOPER_DETAILS['name'],
+        'developer_position': DEVELOPER_DETAILS['position'],
+        'reviewer_name': REVIEWER_DETAILS['name'],
+        'reviewer_position': REVIEWER_DETAILS['position'],
+        'cutoff_dates': get_cutoff_period(),
+        'tc': calculate_cutoffs(DEVELOPER_DETAILS['date_hired'], formatted_datetime),
+        'modules': tasks['Module Label'].unique(),
+        "image_data_array": image_data_array,
+        'tasks': [{key.replace(' ', '_'): value for key, value in record.items()} for record in tasks.to_dict(orient='records')]
+    }
+    doc.render(data)
+
+    # Creates the directory if it does not exist
+    date_today = date.today()
+    directory_path = Path(f".{DIRECTORY_SEPARATOR}charts{DIRECTORY_SEPARATOR}{date_today}")
+
+    if not directory_path.exists():
+        directory_path.mkdir(parents=True)
+
+    doc.save(f"{directory_path}{DIRECTORY_SEPARATOR}{PROJECT_NAME}-output.docx")
 
 
 def generate_project_actual_timeline_as_img(data_frame: pd.DataFrame, project_name: str) -> None:
@@ -109,8 +238,9 @@ def generate_project_projected_timeline_as_img(data_frame: pd.DataFrame, project
     :return: None
     """
 
-    # Have MVP as the first index
-    sorted_list = sorted(data_frame["Module Label"].unique(), key=lambda e: (e != "mvp", e))
+    # Have MVP as the first index and miscellaneous tasks as the last index
+    sorted_list = sorted(sorted(data_frame["Module Label"].unique(), key=lambda e: (e == "mvp", e)),
+                         key=lambda e: (e == "miscellaneous tasks", e))
 
     # Process the data frame with estimates based on the story points into a json
     json_data = []
@@ -132,7 +262,6 @@ def generate_project_projected_timeline_as_img(data_frame: pd.DataFrame, project
         )
 
     new_data_frame = pd.DataFrame(json_data)
-    print(new_data_frame)
 
     # Create figure
     fig = px.timeline(
@@ -223,18 +352,22 @@ def main() -> None:
 
     #  Read the TSV file into a data frame and parse the dates to conform to the proper format
     df = pd.read_table(
-        "egov-bebs.tsv",  # Replace with your project
+        PROJECT_TSV_FILE_PATH,  # Replace with your project
         sep="\t",
         parse_dates=["Date Started", "Date Completed"]
     )
 
-    # Save timeline as image
+    # Generate Accomplishment Report
+    generate_accomplishment_report(data_frame=df, project_name=PROJECT_NAME)
+
+    # Save actual timeline as image
     generate_project_actual_timeline_as_img(data_frame=df, project_name=PROJECT_NAME)
+
+    # Save projected timeline as image
+    generate_project_projected_timeline_as_img(data_frame=df, project_name=PROJECT_NAME)
 
     # Create project statistics
     generate_project_statistics(data_frame=df, project_name=PROJECT_NAME)
-
-    generate_project_projected_timeline_as_img(data_frame=df, project_name=PROJECT_NAME)
 
 
 if __name__ == '__main__':
